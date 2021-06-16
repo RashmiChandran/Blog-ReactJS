0b773351cea53dfb3f30f2632570da6e0d31d939
RashmiChandran <rashmichandran3@gmail.com>
2021-06-16 10:13:26 +0530

    Added basename for browser router

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL >
         <Switch>
         <Route
                 exact
