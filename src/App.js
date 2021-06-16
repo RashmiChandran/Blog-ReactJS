e29da86868253bae3c84780e25ced3f411ba017d
RashmiChandran <rashmichandran3@gmail.com>
2021-06-16 17:08:05 +0530

    Updated API Url

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   }, []);
 
   const fetchArticles = async () =>{
-    const res = await fetch('https://my-json-server.typicode.com/RashmiChandran/Blog-ReactJS/articles')
+    const res = await fetch('http://localhost:4000/articles')
     const data = await res.json();
     return data;
   } 
